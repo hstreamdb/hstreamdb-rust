1d84aebced6d8226756c2c9d8ec4093deddd3dd0
Alissa Tung <alissa-tung@outlook.com>
2022-09-05 12:58:04 +0800

    channel_provider: init (#20)
    * channel_provider: init
    
    * wip

---
+++ src/hstreamdb/src/channel_provider.rs
@@ -0,0 +1,126 @@
+use std::collections::HashMap;
+
+use hstreamdb_pb::h_stream_api_client::HStreamApiClient;
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+use tokio::sync::oneshot;
+use tokio::task::JoinSet;
+use tonic::transport::Channel;
+
+use crate::client::get_available_node_addrs;
+use crate::common;
+
+#[derive(Debug)]
+pub(crate) struct Request(
+    Option<String>,
+    oneshot::Sender<common::Result<HStreamApiClient<Channel>>>,
+);
+
+pub(crate) struct ChannelProvider {
+    request_receiver: UnboundedReceiver<Request>,
+    channels: HashMap<String, HStreamApiClient<Channel>>,
+}
+
+#[derive(Clone)]
+pub(crate) struct Channels(UnboundedSender<Request>);
+
+impl Channels {
+    pub(crate) async fn channel(&self) -> HStreamApiClient<Channel> {
+        let (sender, receiver) = oneshot::channel();
+        let request = Request(None, sender);
+        self.0.send(request).unwrap();
+        receiver.await.unwrap().unwrap()
+    }
+
+    pub(crate) async fn channel_at(
+        &self,
+        url: String,
+    ) -> common::Result<HStreamApiClient<Channel>> {
+        let (sender, receiver) = oneshot::channel();
+        let request = Request(Some(url), sender);
+        self.0.send(request).unwrap();
+        receiver.await.unwrap()
+    }
+
+    pub(crate) fn new(sender: UnboundedSender<Request>) -> Self {
+        Channels(sender)
+    }
+}
+
+impl ChannelProvider {
+    pub(crate) async fn new(
+        channel: &mut HStreamApiClient<Channel>,
+        url_scheme: &str,
+        request_receiver: UnboundedReceiver<Request>,
+    ) -> common::Result<Self> {
+        let channels = get_available_node_addrs(channel, url_scheme)
+            .await?
+            .into_iter()
+            .map(|addr| async move { (addr.clone(), HStreamApiClient::connect(addr).await) })
+            .collect::<Vec<_>>();
+        let mut join_set = JoinSet::new();
+        for channel in channels {
+            join_set.spawn(channel);
+        }
+        let mut channels = HashMap::new();
+        while let Some(channel) = join_set.join_next().await {
+            match channel {
+                Ok(channel) => {
+                    let url = channel.0;
+                    match channel.1 {
+                        Ok(channel) => {
+                            channels.insert(url, channel);
+                        }
+                        Err(err) => {
+                            log::warn!("connect error: url = {url}, {err}")
+                        }
+                    }
+                }
+                Err(err) => {
+                    log::warn!("connect error: {err}")
+                }
+            }
+        }
+        if channels.is_empty() {
+            Err(common::Error::NoAvailableChannel)
+        } else {
+            Ok(ChannelProvider {
+                request_receiver,
+                channels,
+            })
+        }
+    }
+
+    pub(crate) async fn start(&mut self) {
+        while let Some(request) = self.request_receiver.recv().await {
+            match request.0 {
+                Some(url) => match self.channels.get(&url) {
+                    Some(channel) => request
+                        .1
+                        .send(Ok(channel.clone()))
+                        .unwrap_or_else(|err| log::error!("channels reply error: {err:?}")),
+                    None => {
+                        let reply = HStreamApiClient::connect(url.clone())
+                            .await
+                            .map_err(common::Error::TransportError);
+
+                        if let Ok(channel) = &reply {
+                            self.channels.insert(url, channel.clone());
+                        };
+
+                        request
+                            .1
+                            .send(reply)
+                            .unwrap_or_else(|err| log::error!("channels reply error: {err:?}"))
+                    }
+                },
+                None => {
+                    let reply = self.channels.iter().next().unwrap().1.clone();
+                    request
+                        .1
+                        .send(Ok(reply))
+                        .unwrap_or_else(|err| log::error!("channels reply error: {err:?}"))
+                }
+            }
+        }
+    }
+}
