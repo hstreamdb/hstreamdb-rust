bfc8f6fe348b6ff8a18910835747a1a29afd2c31
Alissa Tung <alissa-tung@outlook.com>
2022-10-11 21:47:49 +0800

    producer: fix early remove of deadline (#71)

--- src/hstreamdb/src/flow_controller.rs
+++ src/hstreamdb/src/flow_controller.rs
@@ -100,3 +100,72 @@ impl FlowControllerServer {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::env;
+    use std::time::Duration;
+
+    use hstreamdb_pb::Stream;
+    use hstreamdb_test_utils::rand_alphanumeric;
+
+    use crate::client::Client;
+    use crate::producer::FlushSettings;
+    use crate::{ChannelProviderSettings, Payload, Record};
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_flow_controller() {
+        env_logger::init();
+
+        let addr = env::var("TEST_SERVER_ADDR").unwrap();
+        let mut client = Client::new(
+            addr,
+            ChannelProviderSettings {
+                concurrency_limit: None,
+            },
+        )
+        .await
+        .unwrap();
+        let stream_name = format!("stream-{}", rand_alphanumeric(10));
+        client
+            .create_stream(Stream {
+                stream_name: stream_name.clone(),
+                replication_factor: 1,
+                backlog_duration: 10 * 60,
+                shard_count: 40,
+            })
+            .await
+            .unwrap();
+        let (appender, producer) = client
+            .new_producer(
+                stream_name.clone(),
+                hstreamdb_pb::CompressionType::None,
+                Some(1000000),
+                FlushSettings::builder()
+                    .set_max_batch_len(100)
+                    .set_batch_deadline(1000)
+                    .build(),
+                ChannelProviderSettings {
+                    concurrency_limit: None,
+                },
+                None,
+            )
+            .await
+            .unwrap();
+
+        tokio::spawn(async move {
+            let mut appender = appender;
+            for _ in 0..5000 {
+                appender
+                    .append(Record {
+                        partition_key: rand_alphanumeric(40).to_string(),
+                        payload: Payload::RawRecord(rand_alphanumeric(4000).as_bytes().to_vec()),
+                    })
+                    .await
+                    .unwrap();
+            }
+        });
+        tokio::time::sleep(Duration::from_secs(2)).await;
+        producer.start().await;
+    }
+}
