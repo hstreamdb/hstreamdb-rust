d065d5dbe6baa2d8b776076f924892a6650c8d6e
Alissa Tung <alissa-tung@outlook.com>
2023-01-17 17:56:55 +0800

    refactor: do refine URL and connection settings in channel provider (#99)

--- src/hstreamdb/src/channel_provider.rs
+++ src/hstreamdb/src/channel_provider.rs
@@ -6,6 +6,7 @@ use hstreamdb_pb::h_stream_api_client::HStreamApiClient;
 use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
 use tokio::sync::oneshot;
 use tonic::transport::{Channel, Endpoint};
+use url::Url;
 
 use crate::client::get_available_node_addrs;
 use crate::common;
@@ -114,18 +115,15 @@ impl ChannelProvider {
         let urls = get_available_node_addrs(channel, url_scheme).await?;
         let mut channels = Vec::new();
         for url in urls {
-            match Endpoint::new(url.clone()) {
+            match refine_endpoint(url.clone(), settings.client_tls_config.clone()).await {
                 Err(err) => {
                     log::warn!("create endpoint error: url = {url}, {err}");
                     continue;
                 }
-                Ok(mut endpoint) => {
+                Ok((mut endpoint, _)) => {
                     let uri = endpoint.uri().clone();
                     if let Some(concurrency_limit) = settings.concurrency_limit {
                         endpoint = endpoint.concurrency_limit(concurrency_limit)
-                    }
-                    if let Some(client_tls_config) = settings.client_tls_config.clone() {
-                        endpoint = endpoint.tls_config(client_tls_config)?
                     }
                     match endpoint.connect().await {
                         Err(err) => {
@@ -181,4 +179,40 @@ impl ChannelProvider {
             }
         }
     }
+}
+
+fn set_scheme(url: &str) -> Option<String> {
+    Some(
+        url.replace("hstream://", "http://")
+            .replace("hstreams://", "https://"),
+    )
+}
+
+pub(crate) async fn refine_endpoint<T: AsRef<str>>(
+    url: T,
+    tls_config: Option<ClientTlsConfig>,
+) -> common::Result<(Endpoint, String)> {
+    let url = url.as_ref();
+    Url::parse(url)?;
+    let server_url = set_scheme(url).ok_or_else(|| common::Error::InvalidUrl(url.to_string()))?;
+    let (url_scheme, server_url) = {
+        let mut server_url = Url::parse(&server_url)?;
+        let port = server_url.port();
+        if port.is_none() {
+            server_url
+                .set_port(Some(6570))
+                .map_err(|()| common::Error::InvalidUrl(server_url.to_string()))?;
+        }
+        (server_url.scheme().to_string(), server_url)
+    };
+    log::debug!("client init connect: scheme = {url_scheme}, url = {server_url}");
+
+    let hstream_api_client = {
+        let mut endpoint = Endpoint::new(server_url.to_string())?;
+        if let Some(tls_config) = tls_config {
+            endpoint = endpoint.tls_config(tls_config)?;
+        }
+        endpoint
+    };
+    Ok((hstream_api_client, url_scheme))
 }
