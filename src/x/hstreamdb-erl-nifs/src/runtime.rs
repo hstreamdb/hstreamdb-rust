689cf229882a84c937ed085e61a50500941ff5d4
Alissa Tung <alissa-tung@outlook.com>
2023-01-29 16:49:32 +0800

    erl: avoid timeout alloc

--- src/x/hstreamdb-erl-nifs/src/runtime.rs
+++ src/x/hstreamdb-erl-nifs/src/runtime.rs
@@ -1,4 +1,5 @@
 use std::future::Future;
+use std::time::Duration;
 
 use once_cell::sync::Lazy;
 use tokio::runtime::{self, Runtime};
@@ -15,10 +16,22 @@ static TOKIO_RT: Lazy<Runtime> = Lazy::new(|| {
         .unwrap()
 });
 
-pub(crate) fn spawn<T>(future: T) -> JoinHandle<T::Output>
+fn spawn<T>(future: T) -> JoinHandle<T::Output>
 where
     T: Future + Send + 'static,
     T::Output: Send + 'static,
 {
     TOKIO_RT.spawn(future)
+}
+
+pub(crate) fn spawn_with_timeout<T>(future: T, timeout_value: Option<u64>) -> JoinHandle<()>
+where
+    T: Future<Output = ()> + Send + 'static,
+{
+    match timeout_value {
+        None => spawn(future),
+        Some(timeout) => spawn(async move {
+            if let Ok(()) = tokio::time::timeout(Duration::from_millis(timeout), future).await {}
+        }),
+    }
 }
